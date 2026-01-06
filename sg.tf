80089bb92d5b0cbb6abb2f096682b525d416a449
msabale001 <mahisabale001@gmail.com>
2026-01-06 22:32:36 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -12,11 +12,11 @@ resource "aws_security_group_rule" "eks_cluster_ingress" {
   cidr_blocks       = ["10.0.101.0/24", "10.0.102.0/24"]
   security_group_id = aws_security_group.eks_cluster_sg.id
 }
-resource "aws_security_group_rule" "eks_cluster_egress" {
+resource "aws_security_group_rule" "egress_https" {
   type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"] # VPC CIDR
   security_group_id = aws_security_group.eks_cluster_sg.id
 }
