510a91984edeab69a68dfd95c5d8d75978c3649d
msabale001 <mahisabale001@gmail.com>
2026-01-06 20:51:06 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "eks-cluster-sg"
+  description = "Security group for EKS cluster"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "eks_cluster_ingress" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_cluster_sg.id
+}
+resource "aws_security_group_rule" "eks_cluster_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_cluster_sg.id
+}
